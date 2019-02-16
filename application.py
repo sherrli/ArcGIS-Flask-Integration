068f58ff2bb0739a635f7207cc053b5580f05fd4
Kevin <kevinmschilling@gmail.com>
2019-02-16 12:02:20 +0000

    updating webapp

--- application.py
+++ application.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/smalltrails")
+def smalltrails():
+    return render_template("smalltrails.html")
+
+@app.route("/diseases")
+def diseases():
+    return render_template("diseases.html")
